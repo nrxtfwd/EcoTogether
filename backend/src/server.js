6f78e2dd4a03fb6c30c624ac985c71cc053da027
nrxtfwd <danishazhar2007@gmail.com>
2025-09-22 11:04:28 +0800

    final commit for basse react

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,19 +4,24 @@ import { connectDB } from "./config/db.js"
 import dotenv from "dotenv"
 import rateLimiter from "./middleware/rateLimiter.js"
 import cors from "cors"
+import path from "path"
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
 connectDB()
 
 //middleware
+if (process.env.NODE_ENV !== 'production') {
+    app.use(cors({
+        origin: "http://localhost:5173"
+    }))
+}
+
 app.use(express.json())
-app.use(cors({
-    origin: "http://localhost:5173"
-}))
 //app.use(rateLimiter)
 
 // app.use((req,res,next) => {
@@ -25,6 +30,12 @@ app.use(cors({
 // })
 
 app.use("/api/projects", projectRoutes)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist","index.html"))
+    })
+}
 
 app.listen(process.env.PORT, () => {
     console.log("Server started!", PORT)
